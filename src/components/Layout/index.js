101f92f5fd814f6110be27eed6aeca0be9971ee9
taeveren <nele.taevere@flightcentre.com>
2023-11-19 12:27:01 +1000

    create sidebar

---
+++ src/components/Layout/index.js
@@ -0,0 +1,23 @@
+import { Outlet } from 'react-router-dom';
+import Sidebar from '../Sidebar';
+import './index.scss';
+
+const Layout = () => {
+  return (
+    <div className="app">
+      <Sidebar />
+      <div className="page">
+        <span className="tags top-tags">
+          <Outlet />
+          <span className="tags bottom-tags">
+            &lt;/body&gt;
+            <br />
+            <span className="bottom-tag-html">&lt;/html&gt;</span>
+          </span>
+        </span>
+      </div>
+    </div>
+  )
+}
+
+export default Layout;
